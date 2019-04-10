f3fdda20e8b7058517491b6d3f277b4d3bd5536e
Patrick Sletvold <patricksletvold@hotmail.com>
2019-04-10 20:55:08 +0200

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
+  mode: 'none',
   context: `${__dirname}/src/`,
   devtool: 'source-map',
 
